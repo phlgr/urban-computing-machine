952193cd1ce10eac282fef8340377b145fb2a6c9
phlgr <philippm.gartz@gmail.com>
2020-02-26 14:29:26 +0100

    Clear Result

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -2,9 +2,11 @@
 
 //query elements
 const calculatorOutput = document.querySelector(".calc__output");
-calculatorOutput.value = "Ready to calculate!";
+const calculatorInputs = document.querySelectorAll(".calc__input");
+const calculatorEquals = document.querySelector(".calc__equal");
+const calculatorReset = document.querySelector(".calc__reset");
 
-calculatorOutput.value = subtract(10, 15);
+calculatorOutput.value = "Ready to calculate!";
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -18,3 +20,17 @@ function divide(numberOne, numberTwo) {
 function multiply(numberOne, numberTwo) {
   return numberOne * numberTwo;
 }
+
+const numberOne = calculatorInputs[1].innerText;
+const numberTwo = calculatorInputs[2].innerText;
+
+function clearOutput() {
+  calculatorOutput.value = "0";
+}
+
+function handleClick() {
+  calculatorOutput.value = subtract(numberOne, numberTwo);
+}
+
+calculatorEquals.addEventListener("click", handleClick);
+calculatorReset.addEventListener("click", clearOutput);
