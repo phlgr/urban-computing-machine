d3b6a17262bdc879d2e5d81037a59355bc03ce8a
phlgr <philippm.gartz@gmail.com>
2020-02-26 18:07:19 +0100

    Add all calc functions

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -6,6 +6,11 @@ const calculatorInputs = document.querySelectorAll(".calc__input");
 const calculatorEquals = document.querySelector(".calc__equal");
 const calculatorReset = document.querySelector(".calc__reset");
 const calculatorAdd = document.querySelector(".calc__add");
+const calculatorOperators = document.querySelectorAll(".calc__special");
+
+let oldNumber = 0;
+let newNumber = 0;
+let operator = "";
 
 calculatorOutput.value = "Ready to calculate!";
 
@@ -26,7 +31,13 @@ function clear() {
   calculatorOutput.value = "0";
 }
 
-calculatorReset.addEventListener("click", clear);
+function clearAll() {
+  clear();
+  oldNumber = 0;
+  newNumber = 0;
+}
+
+calculatorReset.addEventListener("click", clearAll);
 
 calculatorInputs.forEach(addInputEventListener => {
   addInputEventListener.addEventListener("click", writeValueToOutput);
@@ -40,4 +51,38 @@ calculatorInputs.forEach(addInputEventListener => {
       calculatorOutput.value += addInputEventListener.innerText;
     }
   }
+});
+
+calculatorOperators.forEach(addOperatorEventListener => {
+  addOperatorEventListener.addEventListener("click", () => {
+    if (oldNumber == 0) {
+      oldNumber = Number(calculatorOutput.value);
+    } else {
+      newNumber = Number(calculatorOutput.value);
+    }
+    operator = addOperatorEventListener.innerText;
+    clear();
+  });
+});
+
+calculatorEquals.addEventListener("click", () => {
+  newNumber = Number(calculatorOutput.value);
+  clear();
+  switch (operator) {
+    case "+":
+      calculatorOutput.value = oldNumber + newNumber;
+      break;
+    case "-":
+      calculatorOutput.value = oldNumber - newNumber;
+      break;
+    case "/":
+      calculatorOutput.value = oldNumber / newNumber;
+      break;
+    case "*":
+      calculatorOutput.value = oldNumber * newNumber;
+      break;
+  }
+
+  oldNumber = Number(calculatorOutput.value);
+  newNumber = 0;
 });
