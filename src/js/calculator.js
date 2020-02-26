0d2a6beeee832ac93c68b5406001ed1c4898a3ab
phlgr <philippm.gartz@gmail.com>
2020-02-26 10:44:55 +0100

    Add calc functions

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -1,1 +1,20 @@
 "use strict";
+
+//query elements
+const calculatorOutput = document.querySelector(".calc__output");
+calculatorOutput.value = "Ready to calculate!";
+
+calculatorOutput.value = subtract(10, 15);
+
+function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
