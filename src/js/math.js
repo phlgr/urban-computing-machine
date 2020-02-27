93bf85ad315d78ce681ff38d0782fc895878bbb8
phlgr <philippm.gartz@gmail.com>
2020-02-27 10:39:26 +0100

    Add math module

---
+++ src/js/math.js
@@ -0,0 +1,12 @@
+export function add(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+export function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+export function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+export function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
